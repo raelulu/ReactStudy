7dff2c2b0584716dd4cc077735d17ebf19d9afc3
raelulu <raelulu23@gmail.com>
2023-07-16 20:29:56 +0900

    faet: create sass & cssinjs

--- Section1/css2-module/src/Button.js
+++ Section1/css2-module/src/Button.js
@@ -3,9 +3,20 @@ import Style from "./Button.module.css";
 import cn from "classnames";
 
 export default function Button({ size }) {
-  if (size === "big") {
-    return <button className={cn(Style.Button, Style.big)}>큰 버튼</button>;
-  } else {
-    return <button className={cn(Style.Button, Style.small)}>작은 버튼</button>;
-  }
+  const isBig = size === "big";
+  return (
+    <button
+      className={cn(Style.Button, {
+        [Style.big]: isBig,
+        [Style.small]: !isBig,
+      })}
+    >
+      {isBig ? "큰 버튼" : "작은 버튼"}
+    </button>
+  );
+  // if (size === "big") {
+  //   return <button className={cn(Style.Button, Style.big)}>큰 버튼</button>;
+  // } else {
+  //   return <button className={cn(Style.Button, Style.small)}>작은 버튼</button>;
+  // }
 }
