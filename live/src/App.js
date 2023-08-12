488a944ab4112a775e57a604c6c15dec86e562d9
raelulu <raelulu23@gmail.com>
2023-08-12 16:00:26 +0900

    feat: context study

--- live/src/App.js
+++ live/src/App.js
@@ -1,17 +1,34 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import React, { useContext, createContext, useState } from "react";
+
+const UserContext = createContext({ username: "unknown", age: 0 });
 
 export default function App() {
+  const [user, setUser] = useState({ username: "mike", age: 23 });
   const [count, setCount] = useState(0);
-  function onClick() {
-    setCount(count + 1);
-    setCount(count + 1);
-  }
-  console.log("render called");
+  console.log("App render");
+
   return (
     <div>
-      <h2>{count}</h2>
-      <button onClick={onClick}>증가</button>
+      <UserContext.Provider value={user}>
+        <Profile />
+        <button onClick={() => setCount(count + 1)}>증가</button>
+      </UserContext.Provider>
     </div>
   );
+}
+
+const Profile = React.memo(function () {
+  console.log("Profile render");
+  return (
+    <div>
+      <Greeting />
+      {/* ... */}
+    </div>
+  );
+});
+
+function Greeting() {
+  console.log("Greeting render");
+  const { username } = useContext(UserContext);
+  return <p>{`${username}님 안녕하세요`}</p>;
 }
