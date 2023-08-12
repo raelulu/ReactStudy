83b570c4833964aa9102a4dd992cbcbc59cc07ea
raelulu <raelulu23@gmail.com>
2023-08-12 14:09:43 +0900

    feat: finish game2048

--- Section3/game2048/src/App.js
+++ Section3/game2048/src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import Header from "./component/Header";
 import AboveGame from "./component/AboveGame";
 import Game from "./component/Game";
+import { useState } from "react";
+import useLocalStorageNumber from "./hook/useLocalStorage";
+import { useEffect } from "react";
 
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useLocalStorageNumber("bestScore", 0);
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+  });
   return (
     <div className="container">
-      <Header />
+      <Header score={score} bestScore={bestScore} />
       <AboveGame />
-      <Game />
+      <Game setScore={setScore} />
     </div>
   );
 }
--- Section3/game2048/src/component/Game.js
+++ Section3/game2048/src/component/Game.js
@@ -3,11 +3,12 @@ import times from "lodash/times";
 import { MAX_POS } from "../constant";
 import { getInitialTileList } from "../util/tile";
 import useMoveTile from "../hook/useMoveTile";
+import Tile from "./Tile";
 
-export default function Game() {
+export default function Game({ setScore }) {
   const [tileList, setTileList] = useState(getInitialTileList);
   //up, down, left, right
-  useMoveTile();
+  useMoveTile({ tileList, setTileList, setScore });
   return (
     <div className="game-container">
       <div className="grid-container">
@@ -22,11 +23,7 @@ export default function Game() {
 
       <div className="tile-container">
         {tileList.map((item) => {
-          <div
-            className={`tile tile-${item.value} tile-position-${item.x} -${item.y}`}
-          >
-            <div className="tile-inner">{item.value}</div>
-          </div>;
+          <Tile key={item.id} {...item} />;
         })}
       </div>
     </div>
