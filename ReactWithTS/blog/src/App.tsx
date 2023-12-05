a1cf67219a12f679192e24d915c4a91e5644bf44
raelulu <raelulu23@gmail.com>
2023-12-05 15:28:14 +0900

    feat: studying fetchAPI

--- ReactWithTS/blog/src/App.tsx
+++ ReactWithTS/blog/src/App.tsx
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import { Header } from 'components/Header';
+import { BlogPost } from 'components/BlogPost';
+import mockPosts from 'mock/posts.json';
+import { Button } from 'components/Button';
+import { Form } from 'components/Form';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #eeeeee;
+  overflow: scroll;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  right: 40px;
+  bottom: 40px;
+`;
+
+interface Post {
+  readonly id: number;
+  readonly userId: number;
+  readonly title: string;
+  readonly body: string;
+}
 
 function App() {
+  const [posts, setPosts] = useState<ReadonlyArray<Post>>([]);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPosts(mockPosts);
+    }, 1000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((responese) => responese.json())
+      .then((json) => setPosts(json))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+      {posts.map((post) => (
+        <BlogPost key={post.id} title={post.title} body={post.body} />
+      ))}
+      <ButtonContainer>
+        <Button label="등록" onClick={() => setShowForm(true)} />
+      </ButtonContainer>
+      {showForm && <Form onClose={() => setShowForm(false)} />}
+    </Container>
   );
 }
 
