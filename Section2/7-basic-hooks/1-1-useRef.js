f828adff74fd1a1bcd0bcc734f61e72217440c5c
raelulu <raelulu23@gmail.com>
2023-12-23 18:47:02 +0900

    feat: Study_useRef

---
+++ Section2/7-basic-hooks/1-1-useRef.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export default function App() {
+  const timerIdRef = useRef(-1);
+  useEffect(() => {
+    timerIdRef.current = setTimeout(() => {}, 1000);
+  });
+
+  useEffect(() => {
+    if (timerIdRef.current >= 0) {
+      clearTimeout(timerIdRef.current);
+    }
+  });
+}
