d281fb48b21b38d6e61ee957cc275c6848d60923
raelulu <raelulu23@gmail.com>
2024-01-03 20:51:43 +0900

    feat: Study_성능최적화 방법2

---
+++ Section4/6-perf2/3.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Parent() {
+  const [selectedFruit, setSelectedFruit] = useState("apple");
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>{`count: ${count}`}</p>
+      <button onClick={() => setCount(count + 1)}>increase count</button>
+      <SelectFruit
+        selected={selectedFruit}
+        onChange={(fruit) => setSelectedFruit(fruit)}
+      />
+    </div>
+  );
+}
+s;
