45510508f5fa3425757f6312abb73beb841ed51b
raelulu <raelulu23@gmail.com>
2024-01-24 23:38:48 +0900

    feat: autocomplete추가

--- whois/src/search/container/SearchInput.js
+++ whois/src/search/container/SearchInput.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AutoComplete, Input } from "antd";
+import { AutoComplete, Input, Space, Typography } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../state";
@@ -22,12 +22,23 @@ export default function Search() {
       style={{
         width: "100%",
       }}
-      options={[]}
+      options={autoCompletes.map((item) => ({
+        value: item.name,
+        label: (
+          <Space>
+            <Typography.Text strong>{item.name}</Typography.Text>
+            <Typography.Text type="secondary">
+              {item.department}
+            </Typography.Text>
+            <Typography.Text>{item.tag}</Typography.Text>
+          </Space>
+        ),
+      }))}
       autoFocus
     >
       <Input
         size="large"
-        placeholder="input here"
+        placeholder="검색어를 입력해주세요"
         prefix={<SearchOutlined />}
       />
     </AutoComplete>
