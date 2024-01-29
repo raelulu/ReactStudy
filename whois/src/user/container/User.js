0d204f44b6f2bee6ef82864caf040489e79f5e55
raelulu <raelulu23@gmail.com>
2024-01-29 14:16:03 +0900

    feat: 사용자 Decription 추가

--- whois/src/user/container/User.js
+++ whois/src/user/container/User.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Col, Row, PageHeader } from "antd";
+import { Col, Row, PageHeader, Descriptions, Typography } from "antd";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../state";
@@ -24,7 +24,20 @@ export default function User({ match }) {
       <Row justify="center">
         <Col xs={24} md={20} lg={14}>
           <PageHeader onBack={history.goBack} title="사용자 정보">
-            {user?.name}
+            {user && (
+              <Descriptions layout="vertical" bordered column={1}>
+                <Descriptions.Item label="이름">
+                  <Typography.Text>{user.name}</Typography.Text>
+                </Descriptions.Item>
+                <Descriptions.Item label="소속">
+                  {user.department}
+                </Descriptions.Item>
+                <Descriptions.Item label="태그">{user.tag}</Descriptions.Item>
+                <Descriptions.Item label="수정 내역">
+                  수정 내역
+                </Descriptions.Item>
+              </Descriptions>
+            )}
           </PageHeader>
         </Col>
       </Row>
