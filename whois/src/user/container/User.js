f6d0d68dad77a274a88735d0700f4028df48ffdf
raelulu <raelulu23@gmail.com>
2024-02-06 20:23:58 +0900

    feat: 사용자 정보 컴포넌트 분리

--- whois/src/user/container/User.js
+++ whois/src/user/container/User.js
@@ -12,6 +12,9 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useDispatch, useSelector } from "react-redux";
 import { Types, actions } from "../state";
 import useFetchInfo from "../../common/hook/useFetchInfo";
+import Department from "./Department";
+import TagList from "./TagList";
+import History from "../../common/component/History";
 
 /**
  * @param {object} param
@@ -49,11 +52,13 @@ export default function User({ match }) {
                   <Typography.Text>{user.name}</Typography.Text>
                 </Descriptions.Item>
                 <Descriptions.Item label="소속">
-                  {user.department}
+                  <Department />
                 </Descriptions.Item>
-                <Descriptions.Item label="태그">{user.tag}</Descriptions.Item>
+                <Descriptions.Item label="태그">
+                  <TagList />
+                </Descriptions.Item>
                 <Descriptions.Item label="수정 내역">
-                  수정 내역
+                  <History />
                 </Descriptions.Item>
               </Descriptions>
             )}
