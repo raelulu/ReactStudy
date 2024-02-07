9ea3a7c9ee401c16f6a7de0dbc3b3424e49b3c87
raelulu <raelulu23@gmail.com>
2024-02-07 10:27:46 +0900

    feat: 사용자 정보 수정기능 추가

--- whois/src/user/state/index.js
+++ whois/src/user/state/index.js
@@ -2,16 +2,25 @@ import {
   createReducer,
   createSetValueAction,
   setValueReducer,
+  FETCH_KEY,
 } from "../../common/redux-helper";
 
 export const Types = {
   SetValue: "user/SetValue",
   FetchUser: "user/FetchUser",
+  FetchUpdateUser: "user/FetchUpdateUser",
 };
 
 export const actions = {
   setValue: createSetValueAction(Types.SetValue),
   fetchUser: (name) => ({ type: Types.FetchUser, name }),
+  fetchUpdateUser: ({ user, key, value, fetchKey }) => ({
+    type: Types.FetchUpdateUser,
+    user,
+    key,
+    value,
+    [FETCH_KEY]: fetchKey,
+  }),
 };
 
 const INITIAL_STATE = {
--- whois/src/user/state/saga.js
+++ whois/src/user/state/saga.js
@@ -1,7 +1,7 @@
-import { all, call, put, takeEvery } from "redux-saga/effects";
+import { all, call, put, takeLeading } from "redux-saga/effects";
 import { Types, actions } from ".";
 import { callApi } from "../../common/util/api";
-import { makeFetchSaga } from "../../common/util/fetch";
+import { deleteApiCache, makeFetchSaga } from "../../common/util/fetch";
 
 function* fetchUser({ name }) {
   const { isSuccess, data } = yield call(callApi, {
@@ -17,11 +17,30 @@ function* fetchUser({ name }) {
   }
 }
 
+function* fetchUpdateUser({ user, key, value }) {
+  const oldValue = user[key];
+  yield put(actions.setValue("user", { ...user, [key]: value }));
+  const { isSuccess, data } = yield call(callApi, {
+    url: "/user/update",
+    method: "post",
+    data: { name: user.name, key, value, oldValue },
+  });
+  if (isSuccess && data) {
+    deleteApiCache();
+  } else {
+    yield put(actions.setValue("user", user));
+  }
+}
+
 export default function* () {
   yield all([
-    takeEvery(
+    takeLeading(
       Types.FetchUser,
       makeFetchSaga({ fetchSaga: fetchUser, canCache: true })
+    ),
+    takeLeading(
+      Types.FetchUpdateUser,
+      makeFetchSaga({ fetchSaga: fetchUpdateUser, canCache: false })
     ),
   ]);
 }
