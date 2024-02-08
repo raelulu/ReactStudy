08639906e09ae06bbf8db9ed39587a388ee41182
raelulu <raelulu23@gmail.com>
2024-02-08 22:36:13 +0900

    feat: TagList작성ing(삭제 기능추가)

--- whois/src/user/container/TagList.js
+++ whois/src/user/container/TagList.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Tag } from "antd";
+import { actions } from "../state";
 
 export default function TagList() {
-  return <div>태그</div>;
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
+  const tags = user?.tag ? user.tag.split(",").map((item) => item.trim()) : [];
+
+  function onDelete(tag) {
+    const newTag = tags.filter((item) => item !== tag).join(", ");
+    dispatch(
+      actions.fetchUpdateUser({
+        user,
+        key: "tag",
+        value: newTag,
+        fetchKey: "tag",
+      })
+    );
+  }
+
+  return (
+    <>
+      {tags.map((item) => (
+        <Tag key={item} closable onClose={() => onDelete(item)}>
+          {item}
+        </Tag>
+      ))}
+    </>
+  );
 }
